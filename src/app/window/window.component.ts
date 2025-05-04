c883f882cea024447ba811b7c261aff2381d6b82
Alexander <alexanderwu271206@gmail.com>
2025-05-04 09:38:38 +0200

    feat: implement window snapping functionality for left and right edges

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -39,6 +39,13 @@ export class WindowComponent implements AfterViewInit {
   lastSize = this.initialSize;
   shouldAnimate = true;
 
+  snapMargin = 20;
+  screenWidth!: number;
+  screeHeight!: number;
+
+  isLeftSnap = false;
+  isRightSnap = false;
+
   windowEl!: HTMLElement;
 
   static currentZIndex = 1;
@@ -52,6 +59,8 @@ export class WindowComponent implements AfterViewInit {
   ) {}
 
   ngAfterViewInit() {
+    this.screenWidth = window.innerWidth;
+    this.screeHeight = window.innerHeight;
     this.windowEl = this.el.nativeElement.querySelector(`.window-${this.id}`);
     const size =
       this.windowData.application === 'Calculator'
@@ -139,7 +148,14 @@ export class WindowComponent implements AfterViewInit {
             if (!dragStarted) dragStarted = true;
 
             this.shouldAnimate = false;
-            this.maximizing = !this.isMaximized && event.client.y < 25;
+            this.isLeftSnap = event.client.x < this.snapMargin;
+            this.isRightSnap =
+              event.client.x > this.screenWidth - this.snapMargin;
+            this.maximizing =
+              !this.isMaximized &&
+              event.client.y < 25 &&
+              !this.isLeftSnap &&
+              !this.isRightSnap;
           },
           end: (event) => {
             dragStarted = false;
@@ -148,6 +164,9 @@ export class WindowComponent implements AfterViewInit {
               if (!this.isMaximized && event.client.y < 25) {
                 this.maximizeWindow(false);
                 this.maximizing = false;
+              }
+              if (this.isLeftSnap || this.isRightSnap) {
+                this.snapWindow();
               }
             }, 0);
           },
@@ -208,6 +227,31 @@ export class WindowComponent implements AfterViewInit {
     interact(this.windowEl).draggable(true);
     interact(this.windowEl).resizable({ enabled: true });
     this.isMaximized = false;
+  }
+
+  snapWindow() {
+    switch (true) {
+      case this.isLeftSnap:
+        this.windowEl.style.transform = `translate(0px, 0px)`;
+        this.windowEl.style.width = '50vw';
+        this.windowEl.style.height = 'calc(100vh - 3.5rem)';
+        this.windowEl.setAttribute('data-x', '0.1');
+        this.windowEl.setAttribute('data-y', '0.1');
+        break;
+      case this.isRightSnap:
+        this.windowEl.style.transform = `translate(50vw, 0px)`;
+        this.windowEl.style.width = '50vw';
+        this.windowEl.style.height = 'calc(100vh - 3.5rem)';
+        this.windowEl.setAttribute(
+          'data-x',
+          (this.screenWidth / 2).toString() + '.1'
+        );
+        this.windowEl.setAttribute('data-y', '0.1');
+        break;
+      default:
+        console.error('Failed to snap window.');
+        break;
+    }
   }
 
   closeWindow() {
