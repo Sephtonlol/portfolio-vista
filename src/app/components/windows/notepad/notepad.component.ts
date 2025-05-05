82d67ef6e2a47915611e7effabbc259a94417668
Alexander <alexanderwu271206@gmail.com>
2025-05-05 08:31:43 +0200

    feat: add character count in notepad

--- src/app/components/windows/notepad/notepad.component.ts
+++ src/app/components/windows/notepad/notepad.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Data } from '../../../interfaces/window.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notepad',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './notepad.component.html',
   styleUrl: './notepad.component.css',
 })
 export class NotepadComponent {
   @Input() data!: Data | undefined;
+
+  contentValue: string = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data'] && this.data?.content) {
+      this.contentValue = this.data.content;
+    }
+  }
 }
